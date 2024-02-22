353690d30eed67dd7638a3d663ef2b017105ff85
samugstu <samuelsouza0225@gmail.com>
2024-02-22 10:40:05 -0300

    adicionar back

--- App.js
+++ App.js
@@ -1,27 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text, View, Image } from 'react-native';
 import styles from './style';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
+  const [jogador, setJogador] = useState(0)
+  const [computador, setComputador] = useState(0)
+  const [placarJ, setPlacarJ] = useState(0)
+  const [placarC, setPlacarC] = useState(0)
+  const [score, setScore] = useState(0)
+
+  function jogar(valor) {
+    setJogador(valor)
+    let computador = Math.floor(Math.random()*3)+1
+    setComputador(computador)
+
+    if (valor==1){
+      if (computador==2){
+        setPlacarJ(placarJ+1)
+      }else if(computador==3){
+        setPlacarC(placarC+1)
+      }else{
+
+      }
+    }else if (valor==2){
+      if (computador==1){
+        setPlacarC(placarC+1)
+      }else if (computador==3){
+        setPlacarJ(placarJ+1)
+      }else{
+
+      }
+    }else if (valor==3){
+      if (computador==1){
+        setPlacarJ(placarJ+1)
+      }else if (computador==2){
+        setPlacarC(placarC+1)
+      }else{
+
+      }
+    }
+
+    if(placarJ==3){
+      setScore(score+1)
+      setPlacarC(0)
+      setPlacarJ(0)
+    }else if (placarC==3){
+      setScore(0)
+      setPlacarC(0)
+      setPlacarJ(0)
+    }else{
+
+    }
+
+  }
+
+  function exibirF(valor){
+    if(valor==1){
+      return (<Image
+        source={require('./assets/img/elementos/ice-removebg-preview.png')}
+        style={styles.fotoElemento}
+      />)
+    }else if (valor==2){
+      return (<Image
+        source={require('./assets/img/elementos/agua.png')}
+        style={styles.fotoElemento}
+      />)
+    }else if(valor==3) {
+      return (<Image
+        source={require('./assets/img/elementos/fire.png')}
+        style={styles.fotoElemento}
+      />)
+    }else {
+      return null
+    }
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={[styles.areaPlacar, styles.shadowAreaElementos]}>
         <View style={styles.areaEsqDir}>
           <View style={styles.areaFoto}>
-
+              
           </View>
           <View style={styles.areaNumPlacar}>
-
+              {placarJ}
           </View>
         </View>
 
         <View style={styles.areaMeio}>
-          <Text style={styles.textPlacar}>X</Text>
+          <Text style={styles.textPlacar}>{score}</Text>
         </View>
 
         <View style={styles.areaEsqDir}>
           <View style={styles.areaNumPlacar}>
-
+              {placarC}
           </View>
           <View style={styles.areaFoto}>
             <View>
@@ -38,7 +113,7 @@ export default function App() {
         <View style={styles.areaJogador}>
           <View style={styles.areaCarta}>
             <View style={[styles.fundoCarta, styles.shadowElementos]}>
-
+              {exibirF(computador)}
             </View>
           </View>
           <View style={styles.areaPinguim}>
@@ -51,31 +126,37 @@ export default function App() {
           </View>
           <View style={styles.areaCarta}>
             <View style={[styles.fundoCarta, styles.shadowElementos]}>
-
+              {exibirF(jogador)}
             </View>
           </View>
         </View>
       </View>
 
       <View style={[styles.areaCartas, styles.shadowElementos]}>
-        <View style={[styles.fundoElementos, styles.shadowElementos]}>
-          <Image
-            source={require('./assets/img/elementos/ice-removebg-preview.png')}
-            style={styles.fotoElemento}
-          />
-        </View>
-        <View style={[styles.fundoElementos, styles.shadowElementos]}>
-          <Image
-            source={require('./assets/img/elementos/agua.png')}
-            style={styles.fotoElemento}
-          />
-        </View>
-        <View style={[styles.fundoElementos, styles.shadowElementos]}>
-          <Image
-            source={require('./assets/img/elementos/fire.png')}
-            style={styles.fotoElemento}
-          />
-        </View>
+        <TouchableOpacity onPress={() => jogar(1)}>
+          <View style={[styles.fundoElementos, styles.shadowElementos]}>
+            <Image
+              source={require('./assets/img/elementos/ice-removebg-preview.png')}
+              style={styles.fotoElemento}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => jogar(2)}>
+          <View style={[styles.fundoElementos, styles.shadowElementos]}>
+            <Image
+              source={require('./assets/img/elementos/agua.png')}
+              style={styles.fotoElemento}
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => jogar(3)}>
+          <View style={[styles.fundoElementos, styles.shadowElementos]}>
+            <Image
+              source={require('./assets/img/elementos/fire.png')}
+              style={styles.fotoElemento}
+            />
+          </View>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
