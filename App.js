e6ff11ce0bf80044ba56dc8102c71ef8b22fe208
Moises <moiseshenryb@gmail.com>
2024-02-21 21:39:23 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, Image } from 'react-native';
+import styles from './style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.areaPlacar, styles.shadowAreaElementos]}>
+        <View style={styles.areaEsqDir}>
+          <View style={styles.areaFoto}>
+
+          </View>
+          <View style={styles.areaNumPlacar}>
+
+          </View>
+        </View>
+
+        <View style={styles.areaMeio}>
+          <Text style={styles.textPlacar}>X</Text>
+        </View>
+
+        <View style={styles.areaEsqDir}>
+          <View style={styles.areaNumPlacar}>
+
+          </View>
+          <View style={styles.areaFoto}>
+            <View>
+              <Image
+                  source={require('./assets/img/pinguim/sensei/sensei-luta.png')}
+                  style={styles.fotoPerfil}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.areaArena}>
+        <View style={styles.areaJogador}>
+          <View style={styles.areaCarta}>
+            <View style={[styles.fundoCarta, styles.shadowElementos]}>
+
+            </View>
+          </View>
+          <View style={styles.areaPinguim}>
+
+          </View>
+        </View>
+        <View style={styles.areaJogador}>
+          <View style={styles.areaPinguim}>
+
+          </View>
+          <View style={styles.areaCarta}>
+            <View style={[styles.fundoCarta, styles.shadowElementos]}>
+
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View style={[styles.areaCartas, styles.shadowElementos]}>
+        <View style={[styles.fundoElementos, styles.shadowElementos]}>
+          <Image
+            source={require('./assets/img/elementos/ice-removebg-preview.png')}
+            style={styles.fotoElemento}
+          />
+        </View>
+        <View style={[styles.fundoElementos, styles.shadowElementos]}>
+          <Image
+            source={require('./assets/img/elementos/agua.png')}
+            style={styles.fotoElemento}
+          />
+        </View>
+        <View style={[styles.fundoElementos, styles.shadowElementos]}>
+          <Image
+            source={require('./assets/img/elementos/fire.png')}
+            style={styles.fotoElemento}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
