dc66208b8beac4a6ef860e146c8f31b553b5f1cb
samugstu <samuelsouza0225@gmail.com>
2024-04-03 16:56:33 -0300

    Perfil tentando

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,39 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, Text, View, Image, TouchableOpacity, ImageBackground, ActivityIndicator } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  ActivityIndicator,
+} from "react-native";
 import styles from "./styles";
 import { useState, useEffect } from "react";
+import { TextInput } from "react-native-web";
 
 export default function App() {
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.background}>
         <View style={styles.header}>
-          <Text>Meu Pinguim</Text>
-          <View style={styles.foto}>
-            <Image
-              source={require("../../../assets/img/pinguim/Club-Penguin.png")}
-              style={styles.fotoP}
-            />
-            <Text style={styles.text}>Nome Pinguim</Text>
-          </View>
-          
-          <View style={styles.level}>
-            <Text >Nivel</Text>
-            <View style={styles.box}>
-                <View>
-
-                </View>
-                <View>
-
-                </View>
+          <View style={styles.area1}>
+            <View style={styles.foto}>
+              <Image
+                source={require("../../../assets/img/pinguim/PVerde.png")}
+                style={styles.photo}
+              />
+            </View>
+            <View style={styles.id}>
+              <View styles={styles.idPlace}></View>
             </View>
           </View>
+          <View style={styles.area2}>
+            <View style={styles.data}>
+              <TextInput style={styles.input} placeholder="Nome" />
+              <TextInput style={styles.input} placeholder="Email" />
+            </View>
+            <View style={styles.data}></View>
+          </View>
         </View>
-        <View style={styles.dados}>
-
+        <View style={styles.dados}></View>
+        <View style={styles.cores}>
+          <View style={styles.cor}>a</View>
+          <View style={styles.cor}>a</View>
+          <View style={styles.cor}>a</View>
+          <View style={styles.cor}>a</View>
         </View>
       </View>
     </SafeAreaView>
-  )
+  );
 }
--- src/pages/Perfil/styles.js
+++ src/pages/Perfil/styles.js
@@ -1,56 +1,78 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-
-  background:{
-    flex:1,
-    backgroundColor:'#007AD7',
+  background: {
+    flex: 1,
+    backgroundColor: "#007AD7",
   },
   header: {
-    flex:1,
-    backgroundColor: 'red',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 2,
+    backgroundColor: "red",
+    flexDirection: "row",
+  },
+  area1: {
+    flex: 2,
+    borderWidth: 3,
+    borderColor: "white",
+  },
+  foto:{
+    flex: 3,
+    borderWidth: 3,
+    borderColor: "white",
+    padding: 10
+  },
+  photo:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderWidth: 2,
+    borderColor: 'blue'
+  },
+  id:{
+    flex: 1,
+  },
+  idPlace:{
+    flex: 1,
+    backgroundColor: 'purple'
+  },
+  area2: {
+    flex: 3,
+    padding: 20
+  },
+  data:{
+    flex: 1
+  },
+  input: {
+    flex: 0.5,
+    width: "100%",
+    color: "#0655BD",
+    backgroundColor: "white",
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    paddingHorizontal: 10,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 17,
+    marginBottom: 5
   },
   dados: {
-    flex:1,
-    backgroundColor: 'green'
+    flex: 3,
+    backgroundColor: "green",
   },
-  foto: {
+  cores: {
     flex: 1,
-    backgroundColor: 'pink',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "blue",
+    flexDirection: 'row',
   },
-  level:{
+  cor:{
     flex: 1,
-    width:'100%',
-    display: 'flex',
-    alignItems: 'center'
-  },
-  fotoP: {
-    width: 150,
-    height: 150,
-    resizeMode: 'contain',
-    borderRadius: '50%',
-    borderWidth: 3,
+    borderWidth: 2,
     borderColor: 'red',
-    marginVertical: 10
+    padding: 10,
   },
-  text: {
-    textTransform: 'uppercase',
-    fontSize: 25
-  },
-  box: {
-    width:'90%',
-    height:'80%',
-    backgroundColor: 'purple',
-    borderRadius: 25
-  }
-
-})
+});
